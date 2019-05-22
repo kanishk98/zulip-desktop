521b670fb95ed5fce9fa0d72e6c457bb9cf8aa5e
Kanishk Kakar <kanishk.kakar@gmail.com>
2019-05-22 22:57:04 +0530

    settings: Show not-allowed cursor for admin fields

--- app/renderer/js/pages/preference/base-section.js
+++ app/renderer/js/pages/preference/base-section.js
@@ -3,15 +3,21 @@
 const {ipcRenderer} = require('electron');
 
 const BaseComponent = require(__dirname + '/../../components/base.js');
+const EnterpriseUtil = require(__dirname + '/../../utils/enterprise-util.js');
 
 class BaseSection extends BaseComponent {
 	generateSettingOption(props) {
-		const {$element, value, clickHandler} = props;
+		const {$element, key, value, clickHandler} = props;
 
 		$element.innerHTML = '';
 
 		const $optionControl = this.generateNodeFromTemplate(this.generateOptionTemplate(value));
 		$element.appendChild($optionControl);
+
+		if (key !== undefined && EnterpriseUtil.isAdminOnly(key)) {
+			$optionControl.getElementsByClassName('toggle')[0].classList.add('toggle-disabled');
+			return;
+		}
 
 		$optionControl.addEventListener('click', clickHandler);
 	}
