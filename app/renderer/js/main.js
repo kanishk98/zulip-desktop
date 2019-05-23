e5b35dbb435f5c47d73b8e0a178ae10aacbafe93
Kanishk Kakar <kanishk.kakar@gmail.com>
2019-05-23 19:25:54 +0530

    enterprise: Add support for preset orgs
    Enable configuration of preset organizations in the enterprise config
    file and load them when app loads.
    Partially valid array of URLs accepted while loading app.

--- app/renderer/js/main.js
+++ app/renderer/js/main.js
@@ -60,6 +60,7 @@ class ServerManagerView {
 		this.loadProxy().then(() => {
 			this.initDefaultSettings();
 			this.initSidebar();
+			this.initPresetOrgs();
 			this.initTabs();
 			this.initActions();
 			this.registerIpcs();
@@ -157,6 +158,35 @@ class ServerManagerView {
 	initSidebar() {
 		const showSidebar = ConfigUtil.getConfigItem('showSidebar', true);
 		this.toggleSidebar(showSidebar);
+	}
+
+	async queueDomain(domain) {
+		try {
+			const serverConf = await DomainUtil.checkDomain(domain);
+			await DomainUtil.addDomain(serverConf);
+			return true;
+		} catch (err) {
+			logger.error(err);
+			logger.error('Could not add ' + domain + '. Please contact your system administrator.');
+			return false;
+		}
+	}
+
+	async initPresetOrgs() {
+		const presetOrgs = EnterpriseUtil.getConfigItem('presetOrganizations', []);
+		// set to true if at least one new domain is added
+		const domainPromises = [];
+		for (const url in presetOrgs) {
+			if (DomainUtil.duplicateDomain(presetOrgs[url])) {
+				continue;
+			}
+			domainPromises.push(this.queueDomain(presetOrgs[url]));
+		}
+		const domainsAdded = await Promise.all(domainPromises);
+		if (domainsAdded.includes(true)) {
+			// at least one domain was resolved
+			ipcRenderer.send('reload-full-app');
+		}
 	}
 
 	initTabs() {
