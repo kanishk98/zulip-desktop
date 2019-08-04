6254ef973d98c31425c1d371440746b1aaaa58a6
vsvipul <vsvipul555@gmail.com>
2019-08-04 16:00:11 +0530

    BrowserView: Show views only when dom content loaded.
    Adds a refresh function which refreshes view
    every 200 ms, and shows it only if the view
    DOM content is loaded. Also, gets rid of buggy
    browser view switching which we were facing earlier.

--- app/main/index.ts
+++ app/main/index.ts
@@ -268,7 +268,7 @@ app.on('ready', () => {
 	});
 
 	ipcMain.on('fix-bounds', () => {
-		ViewManager.fixBounds();
+		ViewManager.fixBounds(mainWindow);
 	});
 
 	ipcMain.on('update-badge', (_event: Electron.IpcMessageEvent, messageCount: number) => {
